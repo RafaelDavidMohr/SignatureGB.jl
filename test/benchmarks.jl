0bd6322057844eec77fb0044fc5ed55390296d32
Rafael Mohr <Rafael.Mohr@lip6.fr>
2021-09-17 10:26:13 +0200

    added kd-tree benchmark

--- test/benchmarks.jl
+++ test/benchmarks.jl
@@ -2,6 +2,7 @@ using BenchmarkTools
 using SignatureGB
 using Combinatorics
 using Dictionaries
+using Oscar
 
 SG = SignatureGB
 
@@ -33,4 +34,29 @@ function sliceddict_benchmark()
     suite["plain_dict"] = @benchmarkable getindices($(dct_1), filter(a -> a[1] == "a", keys($(dct_1))))
     suite["sliced_dict"] = @benchmarkable $(dct_2)["a"]
     suite, dct_1, dct_2
+end
+
+function kd_tree_benchmark(npols, nmons)
+    suite = BenchmarkGroup()
+    R, vars = PolynomialRing(GF(101), ["x$(i)" for i in 1:32], ordering = :degrevlex)
+    order = SG.Grevlex(32)
+    char = 101
+    ctx = SG.idxsigpolynomialctx(SG.Nmod32Γ(char), order = order)
+    pols = filter(p -> !(iszero(p)), [rand(R, 1:1, 1:10) for _ in 1:npols])
+    sigs = [ctx(i, R(1)) for i in eachindex(pols)]
+    for (p, s) in zip(pols, sigs)
+        ctx(s, p)
+    end
+    exp_type = SG.exponenttype(ctx.po.mo)
+    exps = exp_type[collect(1:10)...]
+    kd_mons = [ctx.po.mo(SG.Monomial(SVector{32, exp_type}([j == i ? rand(exps) : zero(exp_type) for j in 1:32])))
+               for i in [rand(1:32) for _ in 1:16]]
+    kd_tree = SG.Kd_node(kd_mons, SG.pos_type(ctx))
+    [SG.insert_new_basis_element!(ctx, kd_tree, s) for s in sigs]
+    mons = [ctx.po.mo(SG.Monomial(rand(1:10, SVector{32, exp_type}))) for _ in 1:nmons]
+
+    suite["div_query_kd_tree"] = @benchmarkable [SG.div_query($(ctx), $(kd_tree), m) for m in $(mons)]
+    suite["div_query"] = @benchmarkable [filter(be -> SG.divides($(ctx).po.mo, SG.leadingmonomial($(ctx), be), m),
+                                                $(sigs)) for m in $(mons)]
+    suite, ctx, kd_tree, mons, sigs
 end
