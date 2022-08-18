94e209a07cfed48cb3c57a61a75be28045950f2f
Rafael Mohr <Rafael.Mohr@lip6.fr>
2022-08-18 10:37:38 +0200

    update readme

--- src/gen_example_file.jl
+++ src/gen_example_file.jl
@@ -13,20 +13,3 @@ function cyclic(vars)
     push!(pols, prod(vars[i] for i in 1:n)-1)
     return pols
 end
-
-function gen_example_file(I::Vector{MP},
-                          filename::String) where {MP <: AbstractAlgebra.MPolyElem}
-    f = open(string(filename, ".jl"), write = true, create = true)
-    println(f, "using Singular")
-    println(f, "using SignatureGB")
-    R = parent(first(I))
-    vars = ["x$(i)" for i in 1:Singular.nvars(R)]
-    nvr = Singular.nvars(R)
-    println(f, "R, ($(["x$(i), " for i in 1:nvr-1]...), $("x$(nvr)")) = Singular.PolynomialRing(Fp($(Singular.characteristic(R))), $(vars))") 
-    println(f, "I = $(I)")
-    println(f, "comp_ideal = gens(R)")
-    println(f, "SignatureGB.f5(comp_ideal)")
-    println(f, "gb = SignatureGB.f5(I, verbose = true)")
-    println(f, "SignatureGB.is_gb(gb)")
-    close(f)
-end
